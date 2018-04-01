cc571b6ddc70f83dc6ad5459eeb82829392f8e7b
Neel Mehta <neelmehta96@gmail.com>
2018-04-01 16:31:50 -0400

    get migration to work

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 var Evidence = artifacts.require("Evidence");
 
 module.exports = function(deployer) {
-  deployer.deploy(Evidence);
+  //uint _image, int _lat, int _long, uint _price, string _desc, address _receiver, uint _violation_type
+  var image = 5;
+  var lat = 4200000000;
+  var lon = -7300000000;
+  var price = 12345;
+  var desc = "hello";
+  var receiver = 12345678901234567890;
+  var violation_type = 1;
+  deployer.deploy(Evidence, image, lat, lon, price, desc, receiver, violation_type);
 };
