eaf913cfb0cd88780d24c4cfa987828ba3a8eaf7
Neel Mehta <neelmehta96@gmail.com>
2018-04-04 17:28:20 -0400

    update readme

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ var instance;
 Evidence.deployed().then(function(_instance) {
     instance = _instance;
     // return instance.bought.call({from: '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef'});
-    return instance.preview.call({from: '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef'});
+    return instance.preview({from: '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef'});
 }).then(function(result) {
     console.log(result);
 
     return instance.previewed.call({from: '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef'});
 }).then(function(result) {
     console.log(result);
+}).catch(function(err){
+  console.log("ERROR: " + err);
 });
 // .error(function(err){
 //   console.log(err)
