d43b9631fde9baf2fe1bfd54e0ae8420514d3045
Neel Mehta <neelmehta96@gmail.com>
2018-04-07 17:46:42 -0400

    WIP

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var Web3            = require('web3'),
 
 // Set up Truffle stuff
 var provider = new Web3.providers.HttpProvider("http://localhost:7545");
+// this is a web3 instance from https://github.com/ethereum/wiki/wiki/JavaScript-API
+var web3 = new Web3(provider)
 
 var Evidence = contract(EvidenceJSON);
 Evidence.setProvider(provider);
@@ -62,7 +64,7 @@ app.post('/preview', (req, res) => {
       }
     }
     else {
-      res.error("Instance does not exist")
+      res.status(400).send("Instance does not exist")
     }
   }
 
@@ -77,34 +79,24 @@ app.post('/preview', (req, res) => {
 // access with: http POST :3000/purchase
 app.post('/purchase', (req, res) => {
 
-  /** 
-  yo
-  (node:6056) UnhandledPromiseRejectionWarning: TypeError: number.lessThan is not a function
-      at Object.fromDecimal (/Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/utils/utils.js:254:19)
-      at /Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/web3/formatters.js:109:30
-      at Array.forEach (<anonymous>)
-      at inputTransactionFormatter (/Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/web3/formatters.js:108:8)
-      at /Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/web3/method.js:89:28
-      at Array.map (<anonymous>)
-      at Method.formatInput (/Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/web3/method.js:88:32)
-      at Method.toPayload (/Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/web3/method.js:114:23)
-      at Eth.send [as sendTransaction] (/Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/web3/method.js:139:30)
-      at SolidityFunction.sendTransaction (/Users/neel/Git/witnesschain-server/node_modules/truffle-contract/node_modules/web3/lib/web3/function.js:173:15)
-  (node:6056) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 3)
-  (node:6056) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
-
-  */
-
   // TODO wrap this all in some meta checking code
   const purchase = async () => {
     if (instance != null) {
       console.log("yo")
-      const purchaseResult = await instance.purchase(
-        {from: '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef',
-        value: new BigNumber("2500000000000000000")})
-      console.log(purchaseResult)
+      try {
+        const purchaseResult = await instance.purchase({
+          from: '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef',
+          value: web3.toWei("1", "ether")
+        })
+        console.log(purchaseResult)
 
-      res.send("BOUGHT")
+        const args = purchaseResult.logs[0].args
+        console.log(args)
+        res.send(`Args is ${args}`)
+      }
+      catch (e) {
+        res.status(400).send("ERROR: " + e)
+      }
 
       // if (previewResult.logs[0]) {
       //   const image = previewResult.logs[0].args.image
@@ -113,16 +105,11 @@ app.post('/purchase', (req, res) => {
       // }
     }
     else {
-      res.error("Instance does not exist")
+      res.status(400).send("Instance does not exist")
     }
   }
 
-  try {
-    purchase()
-  }
-  catch (e) {
-    console.error("Error: " + e)
-  }
+  purchase()
 });
 
 // access with http :3000/previewed
@@ -133,7 +120,7 @@ app.get('/previewed', (req, res) => {
       res.send(`Previewed is ${out}`);
     }
     else {
-      res.error("Instance does not exist")
+      res.status(400).send("Instance does not exist")
     }
   };
 
@@ -141,7 +128,7 @@ app.get('/previewed', (req, res) => {
     previewed()
   }
   catch (e) {
-    res.error("Error: " + e)
+    res.status(400).send("Error: " + e)
   }
 });
 
